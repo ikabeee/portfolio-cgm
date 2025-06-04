7f530dc2540e2287df6e474ffaf284085817bee0
ikabeee <carlglz30@gmail.com>
2025-06-03 23:06:49 -0500

    Add layout components, navigation, and styles; include CVs and favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-      vite: {
+  vite: {
     plugins: [tailwindcss()],
   },
+  experimental: {
+    fonts: [{
+      provider: fontProviders.google(),
+      name: "Poppins",
+      cssVariable: "--font-poppins"
+    }]
+  }
 });
